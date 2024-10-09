d0c1a90748055cbf3f4807a40aa8598c6d00a5ad
Christopher Marcellino Kristiono Putra <excelcmkp638@gmail.com>
2024-10-09 14:06:02 +0700

    Add files via upload

---
+++ 2024-08-15/content.php
@@ -0,0 +1,11 @@
+<?php
+
+    $sekolah="Web SMKN 2 Buduran";
+    $judul="SMKN 2 Buduran";
+    $isi="Ini adalah web SMKN 2 Buduran yang dibuat oleh siswa 11 RPL tahun 2024 tanggal 15-8-24";
+    $hari=["senin","selasa","rabu","kamis","jumat"];
+    $pljrn=["mtk","indo","jepang","jerman"," bhs jawa","agama","inggris","pjok","PKN",
+            "RPL","PKWU","sejarah","BK"];
+    $jadwal="Jadwal Pelajaran RPL 24/25";
+    $img=["logo-ema.PNG","sf24.jpg","sf90.jpg","traktor.jpeg"];
+?>
